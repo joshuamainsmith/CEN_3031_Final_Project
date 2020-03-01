26f00a7b421dfdb578c209a4c2115a9c7e3c2ac2
Stephen McDonald <smm7695>
2020-03-01 17:49:49 -0500

    Initial commit for test branch before installing react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home";
 import NotFound from "./views/NotFound";
 import NavBar from "./components/Header/NavBar";
+import SearchBar from "./components/Header/SearchBar";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <SearchBar />
       <Switch>
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
---
+++ client/src/components/Header/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SearchBar = () => {
+    return (
+        <main>
+        <nav className="topnav">
+            <input type="text" placeholder="Search..."></input>
+        </nav>
+        </main>
+    )
+};
+
+export default SearchBar;
