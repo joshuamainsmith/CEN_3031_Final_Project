9f294dc76d7ea4dc351b3b053060052a9a18c138
Javier Perez <perez.javi.a@gmail.com>
2020-03-08 22:35:55 -0600

    adding routes to react

--- client/src/App.js
+++ client/src/App.js
@@ -2,34 +2,38 @@ import React from 'react';
 import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import Home from './views/Home/Home';
 import NotFound from './views/NotFound';
-import NavBar from './components/Header/NavBar';
+import NavigationBar from './components/Header/NavigationBar';
 import CreateView from './views/CreateView/CreateView';
 import IndexView from './views/IndexView/IndexList';
-import Credit from './components/Footer/Credit';
+// import Credit from './components/Footer/Credit';
 import Search from './views/SearchView/Search';
 import EditView from './views/EditView/EditView';
+import ShowView from './views/ShowView/ShowView';
 
 const App = () => {
 	return (
-		<Router>
-			<div>
-				<NavBar />
-				<Switch>
-					<Route exact path="/">
-						<Redirect to="/Home" />
-					</Route>
-					<Route exact path="/Home" component={Home} />
-					<Route exact path="/Search" component={Search} />
-					<Route exact path="/CreateView" component={CreateView} />
-					<Route exact path="/IndexView" component={IndexView} />
-					<Route exact path="/EditView" component={EditView} />
-					<Route component={NotFound} />
-				</Switch>
-				<footer>
-					<Credit />
-				</footer>
+		<div>
+			<NavigationBar />
+			<div className="container">
+				<Router>
+					<div>
+						<Switch>
+							<Route exact path="/">
+								<Redirect to="/Home" />
+							</Route>
+							<Route exact path="/home" component={Home} />
+							<Route exact path="/careers" component={Search} />
+							<Route exact path="/career" component={CreateView} />
+							<Route exact path="/career/:id" component={ShowView} />
+							<Route exact path="/career/:id/edit" component={EditView} />
+							<Route component={NotFound} />
+						</Switch>
+						<footer>
+						</footer>
+					</div>
+				</Router>
 			</div>
-		</Router>
+		</div>
 	);
 };
 
