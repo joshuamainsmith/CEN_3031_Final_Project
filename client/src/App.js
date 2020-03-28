589a337a805da571caeaab8014015c276635ad81
Stephen McDonald <smm7695>
2020-03-28 19:27:57 -0400

    Save before importing changes from react-state branch

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import Home from './views/Home/Home';
 import NotFound from './views/NotFound';
 import NavigationBar from './components/Header/NavigationBar';
 import CreateView from './views/CreateView/CreateView';
-//import Credit from './components/footer/Credit';
+import Credit from './components/footer/Credit';
 import Search from './views/SearchView/Search';
 import EditView from './views/EditView/EditView';
 import IndexList from './views/IndexView/IndexList';
 import ShowView from './views/ShowView/ShowView';
+import UserList from './views/IndexView/UserList';
+
 
 const App = () => {
 	return (
@@ -26,12 +28,16 @@ const App = () => {
 							<Route exact path="/career" component={CreateView} />
 							
 							<Route exact path="/career/:id" component={ShowView} /> 
-							<Route exact path="/career/:id/edit" component={EditView} />
-							
+							<Route exact path="/career/:id" component={EditView} />
+							<Route exact path="/users" component={UserList} />
+							<Route exact path="/career/:id/edit" component={IndexList} />
+
 							<Route component={NotFound} />
+							
 						</Switch>
 					</div>
 				</Router>
+				<Credit />
 			</div>
 		</div>
 	);
---
+++ client/src/views/IndexView/UserList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserList = () => {
+    return(
+        <div>This is the user list view</div>
+    );
+};
+
+export default UserList;
