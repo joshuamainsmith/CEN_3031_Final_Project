4bb3eaaa47eabc778793803a4e4d910afb82c066
joshuamainsmith <joshuamainsmith@gmail.com>
2020-04-05 13:10:31 -0700

    Stylized nav bar, margin of body, and added footer

--- client/src/components/footer/Credit.js
+++ client/src/components/footer/Credit.js
@@ -1,11 +1,12 @@
 //This file is to give the credit to the project 3 team at UF
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Credit.css';
 
 //Hook function
-function Credit() {
+const Credit = () => {
 	return (
-		<div className="fixed-bottom ">
+		<div className="fixed-bottom f-sty">
 			<div className="Credit">
 				<h1>Florida Middle School Career's Website</h1>
 				<p>Career's Website Designed by the studens in the CEN3031 class at University of Florida.</p>
