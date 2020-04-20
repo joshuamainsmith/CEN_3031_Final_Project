7724b0b5cc1dadde6e22bc70d70b5a78082fe422
Javier Perez <perez.javi.a@gmail.com>
2020-04-19 18:11:51 -0600

    adding new cookie extractor code

--- client/src/Services/AuthService.js
+++ client/src/Services/AuthService.js
@@ -39,14 +39,8 @@ export default {
     return fetch('/api/users/authenticated')
       .then(res => {
         if(res.status !== 401) {
-          console.log("AuthService: not 401, authenticated")
           return res.json().then(data => data);
         } else {
-          // console.log("AuthService: 401, not authenticated. response: " + res.json())
-          // return { isAuthenticated: false, user: {username: "", role: ""}}
-          res.json().then(data =>
-            console.log("AutheService: 401, not authenticated. data: " + data)
-          );
           return { isAuthenticated: false, user: {username: "", role: ""}}
         }
       })
